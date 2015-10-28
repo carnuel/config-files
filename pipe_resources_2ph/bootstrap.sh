0f260516c78f217cb9420f7c95f1d031c7745a18
Carlo Kurth <s4cakur@uni-trier.de>
2015-10-28 12:35:22 +0100

    added config files for multiple slaves per host, not ready yet

---
+++ pipe_resources_2ph/bootstrap.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+
+# Static global variables
+CONFIG_FILE="/mnt/cluster.cnf"
+
+# Helper functions
+# Check if a given ip is in a valid format
+function valid_ip()
+{
+        # Validate ip pattern
+        local ip=$1
+	local is_valid='false'
+        if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+                OIFS=$IFS
+                IFS='.'
+                ip=($ip)
+                IFS=$OIFS
+		if [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]; then
+			is_valid='true'
+		else
+			is_valid='false'
+		fi
+        fi
+	echo "$is_valid"
+}
+
+# Log messages with different log levels
+function log()
+{
+	local log_args=( $@ )
+	local dt=$(date '+%y/%m/%d %H:%M:%S')
+	if [[ $(( ${#log_args[@]} )) > 1 ]]; then
+		level=$1
+
+		prefix=""
+		if [[ $level == 0 ]]; then
+			prefix="INFO"
+		elif [[ $level == 1 ]]; then
+                        prefix="WARN"
+		elif [[ $level == 2 ]]; then
+                        prefix="ERROR"
+		elif [[ $level == 3 ]]; then
+                        prefix="FATAL"
+		fi
+
+		echo "$dt $prefix $2"
+	fi
+}
+
+# Ending the script
+function terminate()
+{
+	if [[ $1 == true ]]; then
+        	log 0 "Ending script."
+		exit 0
+	fi
+}
+
+end_script=false
+# Check if cluster.cnf exists
+if [ ! -f $CONFIG_FILE ]; then
+	log 3 "No cluster configuration found at /mnt/cluster.cnf."
+	end_script=true
+fi
+if [[ $1 != "-bash" && $1 != "-d" && $1 != "-pipework" ]]; then
+	log 3 "Not started with -bash or -d or -pipework."
+	end_script=true
+fi
+
+#check for pipework command errors
+if [[ $1 == "-pipework" ]]; then
+	PIPEWORK='true'
+	if [[ $(valid_ip $2) == false ]]; then
+		log 3 "-pipework ip is not valid. Check ip in -pipework [IP] [COMMAND] for spelling error"
+		end_script=true
+        fi
+	if [[ $3 != "hold" && $3 != "start" ]]; then
+		log 3 "-pipework command has to be either start or hold. Check command in -pipework [IP] [COMMAND] for spelling error"
+		end_script=true
+        fi
+    if [[ $3 != "start" && $4 == "sleep" ]]; then
+		log 3 "-pipework optional command sleep can only follow on command start. Check command in -pipework [IP] [COMMAND] [Optional] for spelling error"
+		end_script=true
+        fi
+else
+	PIPEWORK='false'
+fi
+
+terminate $end_script
+# Variables
+master_hostname=""      # Master hostname
+master_ip=""            # Master IP
+delimiter=";"           # Delimiter to split host string
+slaves=()		# Slave host string for /etc/hosts
+
+# Process cluster configuration
+for host in $(cat $CONFIG_FILE)
+do
+        # e.g. 136.199.51.110;ssds110.dbnet.syssoft.uni-trier.de;master
+        # Host options
+        host_ip=""
+        host_name=""
+        host_type=""
+
+	# Split the host string
+        IFS=$delimiter read -ra host_split <<< "$host"
+	host_split_len=$(( ${#host_split[@]} ))
+
+	end_script=false
+	# Set host options
+	if [[ $host_split_len == 0 ]]; then
+		# Empty line in cluster configuration
+		continue
+	elif [[ $host_split_len > 1 ]]; then
+        	host_ip=${host_split[0]}
+        	host_name=${host_split[1]}
+		if [[ $host_split_len > 2 ]]; then
+			host_type=${host_split[2]}
+		fi
+	else
+                log 3 "Cluster configuration for $host contains too little options (ip;hostname;?type). Remove or extend the host options."
+                end_script=true
+	fi
+
+	# Check host options
+        if [[ $(valid_ip $host_ip) == false ]]; then
+		log 3 "$host is no valid ip. Check cluster configuration."
+		end_script=true
+        fi
+        if [[ $host_ip == "" ]]; then
+		log 3 "No hostname for host $host."
+		end_script=true
+        fi
+
+	# In case of type master set the master ip and hostname.
+	if [[ $host_type == "master" ]]; then
+		if [[ $master_ip == "" && $master_hostname == "" ]]; then
+			master_ip=$host_ip
+			master_hostname=$host_name
+		else
+			log 3 "Multiple master hosts assigned. Check cluster configuration."
+			end_script=true
+		fi
+	# In other cases add a new slave to the array
+	else
+		slaves[$(( ${#slaves[@]} ))]=$(printf "$host_ip\t$host_name")
+	fi
+	# End script when ip or hostname is missing or invalid or when there are multiple master hosts chosen.
+        terminate $end_script
+done
+
+end_script=false
+# Check master options
+if [[ $master_ip == "" ]]; then
+	log 3 "No master ip assigend."
+	end_script=true
+fi
+if [[ $master_hostname == "" ]]; then
+        log 3 "No master hostname assigned."
+        end_script=true
+fi
+if [[ $(( ${#slaves[@]} )) == 0 ]]; then
+	log 3 "No slaves assigned."
+	end_script=true
+fi
+# End script if no master or slaves are given
+terminate $end_script
+
+# Edit xml.templates
+sed s/HOSTNAME/$master_ip/ $HADOOP_PREFIX/etc/hadoop/core-site.xml.template > $HADOOP_PREFIX/etc/hadoop/core-site.xml
+sed s/HOSTNAME/$master_hostname/ $HADOOP_PREFIX/etc/hadoop/yarn-site.xml.template > $HADOOP_PREFIX/etc/hadoop/yarn-site.xml
+sed s/HOSTNAME/$master_hostname/ $HADOOP_PREFIX/etc/hadoop/mapred-site.xml.template > $HADOOP_PREFIX/etc/hadoop/mapred-site.xml
+
+#if [[ $3 == "start" ]]; then
+#	sed s/HOSTNAME/$2/ $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml.template > $HADOOP_PREFIX/etc/hadoop/hdfs-site.xml
+#fi
+log 0 "XML templates edited."
+
+if [[ $PIPEWORK == 'false' || $3 == "start" ]]; then
+
+# Start ssh
+	service ssh start
+	log 0 "SSH started."
+	var_eth0_ip ="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+	# ip of this host
+	if [[ $1 == "-pipework" ]]; then
+		var_ip=$2
+		else
+		var_ip=$var_eth0_ip
+	fi
+	# Start editing /etc/hosts in a temporary file
+	# Add second localhost hostname
+	# Remove 127.0.1.1
+	echo '127.0.0.1	localhost	localhost' > /tmp/hosts
+	log 0 "Changed 127.0.1.1 to $var_ip."
+    # Create the master entry for /etc/hosts
+	master_entry=$(printf "$master_ip\t$master_hostname")
+	# Add the master host to /etc/hosts
+	echo "${master_entry}" >> /tmp/hosts
+	log 0 "$master_entry added to /tmp/hosts."
+	# Add all slaves to /etc/hosts
+	for slave_entry in  "${slaves[@]}"
+		do
+		# Add slave host to /etc/hosts
+		echo "${slave_entry}" >> /tmp/hosts
+		log 0 "$slave_entry added to /tmp/hosts."
+	done
+	# Replace the original /etc/hosts
+	cp /tmp/hosts /etc/hosts
+	log 0 "Replaced /etc/hosts."
+	# Set master mode
+	if [[ $var_ip == $master_ip ]]; then		
+		# Formatting namenode, starting namenode and resourcemanager
+			$HADOOP_PREFIX/bin/hdfs namenode -format -force
+		log 0 "Namenode formatted."
+			$HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
+			log 0 "Namenode started."
+			$HADOOP_PREFIX/sbin/yarn-daemon.sh start resourcemanager
+		log 0 "Resourcemanager started."
+	# Set slave mode
+	else
+		# Starting datanode and nodemanager
+		$HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+			log 0 "Datanode started."
+		$HADOOP_PREFIX/sbin/yarn-daemon.sh start nodemanager
+		log 0 "Nodemanager started."
+	fi
+fi
+# Run container
+log 0 "Running container ..."
+if [[ $1 == "-d" || $3 == "hold" || $4 == "sleep" ]]; then
+	while true; do sleep 1000; done
+elif [[ $1 == "-bash" || $3 == "start" ]]; then
+	/bin/bash
+fi
---
+++ pipe_resources_2ph/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /mnt/*-site.* $HADOOP_PREFIX/etc/hadoop/
+#cp /mnt/*-scheduler.* $HADOOP_PREFIX/etc/hadoop/
